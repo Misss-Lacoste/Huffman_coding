c60ebfd42a953ac4c09a8177e0f59d462b775960
Vllad3 <vlad.kiselv.2006@mail.ru>
2025-02-16 22:47:21 +0300

    to count frequency every element

--- main.cpp
+++ main.cpp
@@ -1,4 +1,15 @@
 
 int main() {
 
+    std::map<char, int> map;
+    for (int i = 0; i < str.length(); i++) {
+        char c = str[i];
+        map[c]++;
+    }
+
+    std::map<char, int>::iterator i;
+    for (i = map.begin(); i != map.end(); i++) {
+        std::cout << i->first << ":" << i->second << std::endl;
+    }
+    return 0;
 }
