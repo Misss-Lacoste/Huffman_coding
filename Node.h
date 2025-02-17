b616fc66b75963cd323a3ebde9eac3818f061625
Vllad3 <vlad.kiselv.2006@mail.ru>
2025-02-17 17:46:29 +0300

    add constructors in Node.h

--- Node.h
+++ Node.h
@@ -5,4 +5,10 @@ public:
     int value;
     char letter;
     Node *left, *right;
+    Node() {}
+    Node(Node *left, Node *right) {
+        this->left = left;
+        this->right = right;
+        value = this->left->value + this->right->value;
+    }
 };
