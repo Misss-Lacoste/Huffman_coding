48990119e762a97314a3efc42568ea00f24a4a3b
Pavel Khlunev <96428376+pkhlunev@users.noreply.github.com>
2025-02-19 18:00:41 +0300

    Update main.cpp adding decompress

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
+#include "Node.h"
 #include "functions.h"
 #include <iostream>
 #include <string>
 #include <vector>
 #include <map>
 #include <list>
-
+#include <cstdbool>
 
 std::vector<bool> code;
 std::map<char, std::vector<bool> > table;
@@ -20,8 +21,10 @@ int main() {
 
     build_map(list, map);
 
-    Node *root = create_tree(list);
+    Node* root = create_tree(list);
     build_table(root, code, table);
+
+    std::vector<bool> encodedBits;
 
     for (int i = 0; i < str.length(); i++) {
         char c = str[i];
@@ -29,31 +32,16 @@ int main() {
 
         for (int j = 0; j < v.size(); j++) {
             std::cout << v[j];
+            encodedBits.push_back(v[j]);
         }
     }
 
     std::cout << std::endl;
 
     print_table(table);
-    
-    // IMPORTANT:  Memory cleanup to prevent memory leaks
 
-    // Add root to list
-    std::list<Node*> nodesToDelete;
-    nodesToDelete.push_back(root);
+    std::string decodedStr = decode_sequence(root, encodedBits);
+    std::cout << "Decoded string: " << decodedStr << std::endl;
 
-    while(!nodesToDelete.empty()){
-        Node* current = nodesToDelete.front();
-        nodesToDelete.pop_front();
-
-        if(current->left){
-            nodesToDelete.push_back(current->left);
-        }
-        if(current->right){
-            nodesToDelete.push_back(current->right);
-        }
-        delete current;
-    }
     return 0;
-
 }
