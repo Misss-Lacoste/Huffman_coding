d430a042db27b7a9b33418709c2b74f93045e870
Pavel Khlunev <96428376+pkhlunev@users.noreply.github.com>
2025-02-19 19:14:02 +0300

    fixing the "t" problem

--- main.cpp
+++ main.cpp
@@ -5,11 +5,17 @@
 #include <vector>
 #include <map>
 #include <list>
-#include <cstdbool>
-
+#include <fstream>
 std::vector<bool> code;
-std::map<char, std::vector<bool> > table;
+std::map<char, std::vector<bool>> table;
 
+void deleteTree(Node* node) {
+    if (node) {
+        deleteTree(node->left);
+        deleteTree(node->right);
+        delete node;
+    }
+}
 
 int main() {
     std::string str = "it is my striiiiing!!!!";
@@ -18,30 +24,74 @@ int main() {
     frequency(map, str);
 
     std::list<Node*> list;
-
     build_map(list, map);
 
     Node* root = create_tree(list);
+
     build_table(root, code, table);
 
     std::vector<bool> encodedBits;
 
-    for (int i = 0; i < str.length(); i++) {
+    for (size_t i = 0; i < str.length(); i++) {
         char c = str[i];
         std::vector<bool> v = table[c];
-
-        for (int j = 0; j < v.size(); j++) {
-            std::cout << v[j];
-            encodedBits.push_back(v[j]);
-        }
+        encodedBits.insert(encodedBits.end(), v.begin(), v.end());
     }
 
-    std::cout << std::endl;
+    // Запись количества закодированных битов в файл
+    size_t numBits = encodedBits.size();
+    std::ofstream outfile("encoded.bin", std::ios::binary);
+    outfile.write(reinterpret_cast<const char*>(&numBits), sizeof(numBits));
 
-    print_table(table);
+    char byte = 0;
+    int count = 0;
 
-    std::string decodedStr = decode_sequence(root, encodedBits);
-    std::cout << "Decoded string: " << decodedStr << std::endl;
+    for (size_t i = 0; i < encodedBits.size(); ++i) {
+        byte = byte | (encodedBits[i] << (7 - count));
+        ++count;
+        if (count == 8) {
+            outfile.write(&byte, 1);
+            count = 0;
+            byte = 0;
+        }
+    }
+    if (count != 0) {
+        outfile.write(&byte, 1);
+    }
+    outfile.close();
+
+    std::vector<bool> encodedBitsFromFile;
+
+    // Считывание количества закодированных битов из файла
+    size_t numBitsFromFile = 0;
+    std::ifstream infile("encoded.bin", std::ios::binary);
+    infile.read(reinterpret_cast<char*>(&numBitsFromFile), sizeof(numBitsFromFile));
+
+    char readByte;
+    while (infile.read(&readByte, 1) && encodedBitsFromFile.size() < numBitsFromFile) {
+        for (int i = 7; i >= 0; --i) {
+            bool bit = (readByte >> i) & 1;
+            encodedBitsFromFile.push_back(bit);
+        }
+    }
+    infile.close();
+
+    // Ограничить количество бит, обрабатываемых для декодирования
+    encodedBitsFromFile.resize(numBitsFromFile);
+
+    std::string decodedStr = decode_sequence(root, encodedBitsFromFile);
+    std::cout << "decoded" << decodedStr << std::endl;
+
+    std::ofstream outFile("decompressed_output.txt");
+    if (!outFile) {
+        std::cerr << "cant open for writing" << std::endl;
+        return 1;
+    }
+    outFile << decodedStr;
+    outFile.close();
+    std::cout << "data has written to decompressed_output.txt" << std::endl;
+
+    deleteTree(root);
 
     return 0;
 }
